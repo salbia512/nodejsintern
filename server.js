72352f5e900e03461a43eec19659da3fd86e2733
salbia512 <wajdanalik512@gmail.com>
2025-12-28 20:32:53 +0500

    deploy abckend to vercel

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
+// require("dotenv").config();
+// const app = require("./app");
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
