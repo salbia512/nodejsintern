18dceeb7a742befc55092a132c87f7a4b42438d5
salbia512 <wajdanalik512@gmail.com>
2026-01-01 17:49:03 +0500

    adding safely

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ status: "Serverless works ✅" });
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB error:", err);
+    throw err;
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    await connectDB();
+    res.json({ status: "DB connected ✅" });
+  } catch (error) {
+    res.status(500).json({ status: "DB failed ❌" });
+  }
 });
 
 module.exports = app;
