ae1426681226a0aba27f3339b092d431f6c1d8dd
salbia512 <wajdanalik512@gmail.com>
2026-01-01 17:40:14 +0500

    test

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
+const express = require("express");
+const app = express();
 
-// require("dotenv").config();
-// const app = require("./app");
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-require("dotenv").config();
-const app = require("./app");
+app.get("/", (req, res) => {
+  res.json({ status: "Serverless works ✅" });
+});
 
 module.exports = app;
