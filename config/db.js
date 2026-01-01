d55bcc72ddf519af525315e1b397a1d03cbd11f4
salbia512 <wajdanalik512@gmail.com>
2026-01-01 17:30:36 +0500

    fixing error

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-   await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Atlas Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(error.message);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
